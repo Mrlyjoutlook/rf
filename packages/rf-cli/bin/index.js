67182f0a9045e32f9c44be160cc79a9a543c53b4
git_outlook <mr.lyj@outlook.com>
2018-06-05 11:38:56 +0800

    add lint

--- packages/rf-cli/bin/index.js
+++ packages/rf-cli/bin/index.js
@@ -1,21 +1,21 @@
 #!/usr/bin/env node
-'use strict';
+"use strict";
 
-const path = require('path');
-const fs = require('fs-extra');
-const program = require('commander');
-const chalk = require('chalk');
-const spawn = require('cross-spawn');
-const pg = require('../package.json');
+const path = require("path");
+const fs = require("fs-extra");
+const program = require("commander");
+const chalk = require("chalk");
+const spawn = require("cross-spawn");
+const pg = require("../package.json");
 
 function wrap(sp) {
-  sp.on('close', function(code) {
+  sp.on("close", function(code) {
     process.exit(code);
   });
 }
 
 function executable(subcmd) {
-  var file = path.join(__dirname, subcmd + '.js');
+  var file = path.join(__dirname, subcmd + ".js");
   if (fs.existsSync(file)) {
     return file;
   }
@@ -27,37 +27,37 @@ program.version(pg.version);
 //   .option('-T, --no-tests', 'ignore test hook')
 
 program
-  .command('new')
-  .description('构建项目工程')
-  .usage('<project name>')
+  .command("new")
+  .description("构建项目工程")
+  .usage("<project name>")
   .action(function(cmd) {
-    console.log(chalk.green('peak: build project.'));
+    console.log(chalk.green("peak: build project."));
     /**
      * 判断是否存在文件
      * 是 -> 提示
      * 否 -> 执行命令
      */
     if (fs.existsSync(cmd)) {
-      console.log(chalk.red('peak: project name is exist!'));
+      console.log(chalk.red("peak: project name is exist!"));
       process.exit(1);
     } else {
-      console.log(chalk.green('peak: executing command (new...).'));
+      console.log(chalk.green("peak: executing command (new...)."));
       const args = process.argv.slice(3);
       // const subcmd = program.args[0];
       const runPath = executable(process.argv.slice(2, 3));
-      wrap(spawn(runPath, args, { stdio: 'inherit', customFds: [0, 1, 2] }));
+      wrap(spawn(runPath, args, { stdio: "inherit", customFds: [0, 1, 2] }));
     }
   })
-  .on('--help', function() {
-    console.log('  Examples:');
+  .on("--help", function() {
+    console.log("  Examples:");
     console.log();
-    console.log('    $ peak new react-demo');
+    console.log("    $ peak new react-demo");
     console.log();
   });
 
 program
-  .command('*')
-  .description('无此命令')
+  .command("*")
+  .description("无此命令")
   .action(function(cmd) {
     console.log('peak: deploying "%s"', cmd);
   });
